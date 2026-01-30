a45fa61949ce88794ffa99645bbdf5c53653cab6
terresa-user15 <vcl.com>
2026-01-30 20:22:45 +0000

    added ec2_instance folder

---
+++ ec2-instance/ec2.tf
@@ -0,0 +1,14 @@
+locals {
+ tag_Name = "Viswa-${var.env}"
+}
+resource "aws_instance" "main" {
+ ami = var.region_ami_map[var.region-1]
+ instance_type = var.instance_type
+ subnet_id = var.subnets[count.index]
+ count = var.num_of_vm
+ associate_public_ip_address = false
+ vpc_security_group_ids = var.web_sg
+ tags = {
+ "Name" = local.tag_Name
+ }
+}
