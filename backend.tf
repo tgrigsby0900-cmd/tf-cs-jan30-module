dd08d22b0d677d680709f2596205b79a834e3d02
terresa-user15 <vcl.com>
2026-01-30 21:38:59 +0000

    made changes to vars

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "boa-terraform-state-im-jan30-user15"
+    bucket = "tf-state-user15-290126"
     key    = "dev-cs/terraform.tfstate"
     region = "us-west-1"
     encrypt = true
-    use_lockfile = "terraform-locks-user15"
+   # use_lockfile = "terraform-locks-user15"
   }
 }
